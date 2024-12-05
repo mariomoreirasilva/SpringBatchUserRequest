0eed66d8e987615c94c9081bb73114935615bb8c
mariomoreirasilva <mario@desban.org.br>
2024-12-05 15:53:08 -0300

    Step configurado

---
+++ src/main/java/com/devsuperior/user_rquest_springbatch/step/FetchUserDataAndStoredDBStepConfig.java
@@ -0,0 +1,34 @@
+package com.devsuperior.user_rquest_springbatch.step;
+
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.info.ProjectInfoProperties.Build;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import com.devsuperior.user_rquest_springbatch.dto.UserDTO;
+
+@Configuration
+public class FetchUserDataAndStoredDBStepConfig {
+	
+	@Autowired
+	private PlatformTransactionManager transactionManager;
+	
+	@Value("$(chunkSize)")
+	private int chunkSize;
+	
+	@Bean
+	public Step fetchUserDataAndStoredDBStep(ItemReader<UserDTO> fetchUserDataReader, JobRepository jobRepository) {
+		
+		return new StepBuilder("fetchUserDataAndStoredDBStep", jobRepository)
+				.<UserDTO, UserDTO>chunk(chunkSize, transactionManager)
+				.reader(fetchUserDataReader)
+				.build();
+	}
+
+}
